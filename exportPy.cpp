6a7aa141eb9b9a445a815d17a90d7e622aa74bd0
To-k1 <158008825@qq.com>
2022-04-20 14:58:59 +0800

    first-commit

---
+++ exportPy.cpp
@@ -0,0 +1,25 @@
+﻿#define BOOST_PYTHON_STATIC_LIB
+
+#include <boost/python.hpp>
+#include <iostream>
+#include "Registrating.h"
+
+
+
+
+
+BOOST_PYTHON_MODULE(registration_py)
+{
+	//class of default constructor
+	boost::python::class_<Registrator>("Registrator")
+		.def(boost::python::init<const Registrator&>())
+		.def("isPaused", &Registrator::isPaused)
+		.def("worker_pause", &Registrator::worker_pause)
+		.def("worker_resume", &Registrator::worker_resume)
+		.def("registrating", &Registrator::registrating)
+		.def("handleResults", &Registrator::handleResults)
+		.def("handlePause", &Registrator::handlePause)
+		.def("sendProcess", &Registrator::sendProcess);
+	//.def("print", &Message::get); //不可以使用print这种关键字作为导出函数名
+}
+
---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "Registrating.h"
+#include <string>
+using namespace std;
+
+int main() {
+	string srcPattern = "F:\\shixi\\CS\\registration\\registration\\srcImg";
+	string binPattern = "F:\\shixi\\CS\\registration\\registration\\binImg";
+	string dstPattern = "F:\\shixi\\CS\\registration\\registration\\dstImg";
+	char useSemiAuto = 'n';
+	char useFailedImg = 'n';
+	int startPos = 0;
+	Registrator registrator;
+	registrator.registrating(srcPattern, binPattern, dstPattern, useSemiAuto, useFailedImg, startPos);
+
+	return 0;
+}
